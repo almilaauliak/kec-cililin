e8e44966c779682c0f7331966a4f94fe749f0998
Almila <almilaauliak@gmail.com>
2024-10-10 13:29:39 +0700

    Update baru Almila 10 Okt

---
+++ resources/views/layoutes/main.blade.php
@@ -0,0 +1,200 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>Kecamatan Cililin KBB</title>
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com" rel="preconnect">
+  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Main CSS File -->
+  <link href="assets/css/main.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Moderna
+  * Template URL: https://bootstrapmade.com/free-bootstrap-template-corporate-moderna/
+  * Updated: Aug 07 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body class="index-page">
+
+  <header id="header" class="header d-flex align-items-center fixed-top">
+    <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+
+      <a href="index.html" class="logo d-flex align-items-center">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <!-- <img src="assets/img/logo.png" alt=""> -->
+        <h1 class="sitename">Cililin</h1>
+      </a>
+
+      <nav id="navmenu" class="navmenu">
+        <ul>
+          <li><a href="index.html" class="active">Beranda</a></li>
+          <li class="dropdown"><a href="#"><span>Pemerintahan</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <ul>
+                <li class="dropdown"><a href="#"><span>Camat</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                <ul>
+                  <li><a href="#">PMD</a></li>
+                  <li><a href="#">BINWAS</a></li>
+                  <li><a href="#">PENYALIK</a></li>
+                  <li><a href="#">TRANTIBUM</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a href="#"><span>Sekcam</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                <ul>
+                  <li><a href="#">Sub Bidang Perencanaan dan Keuangan</a></li>
+                  <li><a href="#">Sub Bidang Kepegawaian dan Umum</a></li>
+                </ul>
+              </li>      
+          </li>     
+            </ul>
+            <li class="dropdown"><a href="#"><span>Informasi</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <ul>
+                <li><a href="">Sejarah Kecamatan</a></li>
+                <li><a href="">Struktur Organisasi</a></li>
+                <li><a href="">Maklumat Pelayanan</a></li>
+                <li><a href="">Jam Pelayanan</a></li>
+                </li>
+            </ul>    
+            <li class="dropdown"><a href="#"><span>Desa</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <ul>
+                <li><a href="">Cililin</a></li>
+                <li><a href="">Budiharja</a></li>
+                <li><a href="">Batulayang</a></li>
+                <li><a href="">Bongas</a></li>
+                <li><a href="">Karanganyar</a></li>
+                <li><a href="">Karangtanjung</a></li>
+                <li><a href="">karyamukti</a></li>
+                <li><a href="">Kidangpananjung</a></li>
+                <li><a href="">Mukapayung</a></li>
+                <li><a href="">Nanggerang</a></li>
+                <li><a href="">Rancapanggung</a></li>
+                </li>
+            </ul>  
+
+            <li class="dropdown"><a href="#"><span>Pelayanan</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <ul>
+                <li><a href="">Ahli Waris</a></li>
+                <li><a href="">SKU (Surat Keterangan Usaha)</a></li>
+                <li><a href="">IUM (Ijin Usaha Mikro)</a></li>
+                <li><a href="">SPPT PBB</a></li>
+                <li><a href="">IMB</a></li>
+                <li><a href="">E-KTP</a></li>
+                <li><a href="">Kartu Keluarga</a></li>
+                <li><a href="">Surat Pindah</a></li>
+                <li><a href="">Fatwa Waris</a></li>
+                <li><a href="">SKKB (Surat Keterangan Kelakuan Baik)</a></li>
+                <li><a href="">SKTM (Surat Keterangan Tidak Mampu) </a></li>
+                <li><a href="">Rekomendasi Proposal</a></li>
+                <li><a href="">PPARs</a></li>
+                </li>
+            </ul>  
+
+            <li class="dropdown"><a href="#"><span>Kegiatan</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <ul>
+                <li><a href="">Dokumentasi Kegiatan</a></li>
+                </li>
+            </ul>      
+
+          <li><a href="contact.html">Berita</a></li>
+
+        </ul>
+        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+      </nav>
+
+    </div>
+  </header>
+
+  <main class="main">
+
+  @yield('content')
+
+  </main>
+
+  <footer id="footer" class="footer dark-background">
+
+    
+
+    <div class="container footer-top">
+      <div class="row gy-4">
+        <div class="col-lg-4 col-md-6 footer-about">
+          <a href="index.html" class="d-flex align-items-center">
+            <span class="sitename">Kecamatan Cililin</span>
+          </a>
+          <div class="footer-contact pt-3">
+            <p>Alamat Kantor</p>
+            <p>Jl.Raya Cililin No.1 Desa Cililin, Kab.Bandung Barat</p>
+            <p class="mt-3"><strong>Phone:</strong> <span>no telp</span></p>
+            <p><strong>Email:</strong> <span>info@example.com</span></p>
+          </div>
+        </div>
+        
+        <div class="col-lg-4 col-md-12">
+          <h4>Follow Us</h4>
+          <div class="social-links d-flex">
+            <a href=""><i class="bi bi-twitter-x"></i></a>
+            <a href=""><i class="bi bi-facebook"></i></a>
+            <a href="https://www.instagram.com/kecamatan.cililin"><i class="bi bi-instagram"></i></a>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="container copyright text-center mt-4">
+      <p>© <span>Copyright</span> <strong class="px-1 sitename">Kecamatan Cililin</strong>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you've purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
+        Designed by <a href="">Kecamatan Cililin Website Develop</a>
+      </div>
+    </div>
+
+  </footer>
+
+  <!-- Scroll Top -->
+  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Preloader -->
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+
+  <!-- Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
